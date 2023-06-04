82605ab907b0871b7642fe450915c70363f8cb5e
Jean Carlos Perez <jean.perez@lever.co>
2023-06-04 14:46:03 -0400

    Orders: - redefining the productList structure

--- src/modules/order/model/order.model.js
+++ src/modules/order/model/order.model.js
@@ -1,9 +1,46 @@
 const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
-    productList: [{
-        type: String,
+    productList: {
+        type: [
+            {
+                id: {
+                    type: Number,
+                    required: true
+                },
+                title: {
+                    type: String,
+                    required: true
+                },
+                price: {
+                    type: Number,
+                    required: true
+                },
+                description: {
+                    type: String,
+                    required: true
+                },
+                category: {
+                    type: String,
+                    required: true
+                },
+                image: {
+                    type: String,
+                    required: true
+                },
+                rating: {
+                    rate: {
+                        type: Number,
+                        required: true
+                    },
+                    count: {
+                        type: Number,
+                        required: true
+                    }
+                }
+            }
+        ],
         required: true
-    }],
+    },
     totalValue: {
         type: Number,
         required: true
